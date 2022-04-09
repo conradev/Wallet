baa88efdb162cd45aa96559aa3330348f55671c8
Conrad Kramer <conrad@conradkramer.com>
2022-04-08 17:32:34 -0700

    Update Android Gradle plugin to 7.1.3
    This is a minor version increase suggested by Android Studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath(kotlin("gradle-plugin", findProperty("kotlin.version").toString()))
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
         classpath("org.jmailen.gradle:kotlinter-gradle:3.9.0")
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("com.dorongold.plugins:task-tree:2.1.0")
     }
 }
