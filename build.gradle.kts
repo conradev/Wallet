b2fd165d493666a6afbd8479f1d29ea2015a4611
Conrad Kramer <conrad@conradkramer.com>
2022-06-25 13:40:39 -0700

    Upgrade Android Target SDK to API 32
    Also update a number of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     dependencies {
         classpath(kotlin("gradle-plugin", findProperty("kotlin.version").toString()))
         classpath("app.cash.sqldelight:gradle-plugin:2.0.0-alpha02")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jmailen.gradle:kotlinter-gradle:3.9.0")
-        classpath("com.android.tools.build:gradle:7.2.0")
         classpath("com.dorongold.plugins:task-tree:2.1.0")
     }
 }
