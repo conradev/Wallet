f8b8e5df678a13c0417c4f6b856810ea703e124c
Conrad Kramer <conrad@conradkramer.com>
2022-12-04 22:17:40 -0500

    Introduce android-tools to the Nix flake
    This means that adb will be available in the direnv.

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     nodejs-19_x
     gitlint
+    android-tools
   ];
   shellHook = ''
     export PATH="$PWD/Extension/node_modules/.bin:$PWD/node_modules/.bin:$PATH"
