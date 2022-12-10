f5ee30859d3907538c90ebbbf785f73e82ff9d9b
Conrad Kramer <conrad@conradkramer.com>
2022-12-10 18:36:47 -0500

    Add latest Android SDK and Java to the Nix flake
    This adds more utilities to the shell, like apkanalyzer, and configures
    ANDROID_HOME and JAVA_HOME to point to the latest SDK and Java.

--- flake.nix
+++ flake.nix
@@ -2,11 +2,34 @@
   description = "Wallet";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, android-nixpkgs }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "wallet";
-      shell = ./shell.nix;
+      shell = { pkgs }:
+        let
+          android-stable = pkgs.callPackage android-nixpkgs {
+            channel = "stable";
+          };
+          android-sdk = android-stable.sdk (sdkPkgs: with sdkPkgs; [
+            cmdline-tools-latest
+            build-tools-33-0-0
+            platform-tools
+            platforms-android-33
+          ]);
+        in
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs-19_x
+              pkgs.gitlint
+              pkgs.temurin-bin-19
+              android-sdk
+            ];
+            shellHook = ''
+              export PATH="$PWD/Extension/node_modules/.bin:$PWD/node_modules/.bin:$PATH"
+            '';
+          };
     };
 }
