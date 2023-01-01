e31a1434ce362a63a2ab2b83d10dabce7bf43d9b
Conrad Kramer <conrad@conradkramer.com>
2023-01-01 14:03:08 -0500

    Don't use multiple scenes on iOS
    This commit checks for multiple scene support before requesting a scene
    activation when opening a Location object.

--- Apple/App/iOS/Location+App.swift
+++ Apple/App/iOS/Location+App.swift
@@ -3,13 +3,18 @@ import WalletUICore
 
 extension Location {
     public func open() {
-        let options = UIWindowScene.ActivationRequestOptions()
-        options.preferredPresentationStyle = .prominent
-        UIApplication.shared.requestSceneSessionActivation(
-            nil,
-            userActivity: userActivity,
-            options: options,
-            errorHandler: nil
-        )
+        let application = UIApplication.shared
+        if application.supportsMultipleScenes {
+            let options = UIWindowScene.ActivationRequestOptions()
+            options.preferredPresentationStyle = .prominent
+            application.requestSceneSessionActivation(
+                nil,
+                userActivity: userActivity,
+                options: options,
+                errorHandler: nil
+            )
+        } else {
+            application.open(universalLink)
+        }
     }
 }
