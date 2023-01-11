92c541575fae332c359dd330515b022ffc14f474
Conrad Kramer <conrad@conradkramer.com>
2023-01-11 12:43:04 -0500

    Introduce ERC20 and ERC721 client implementations
    These interfaces are lightweight wrappers around RpcClient that handle
    all of the data marhsalling so that you can call functions on the
    Ethereum world computer just as if they were local functions.

--- Shared/src/commonMain/kotlin/com/conradkramer/wallet/ethereum/abi/ERC165.kt
+++ Shared/src/commonMain/kotlin/com/conradkramer/wallet/ethereum/abi/ERC165.kt
@@ -2,16 +2,14 @@ package com.conradkramer.wallet.ethereum.abi
 
 import com.conradkramer.wallet.encoding.toByteArray
 import com.conradkramer.wallet.ethereum.Address
-import com.conradkramer.wallet.ethereum.Quantity
 import com.conradkramer.wallet.ethereum.RpcClient
 
 internal class ERC165(client: RpcClient, address: Address) : Contract(client, address) {
-    suspend fun supportsInterface(tag: UInt): Boolean {
-        val invocation = Invocation("supportsInterface")
-            .parameter(Type.Bytes(4)) { encode(tag.toByteArray()) }
-        val result: Quantity = this.invoke(invocation)
-        return result.toLong() != 0L
-    }
+    suspend fun supportsInterface(tag: UInt) = invoke(
+        Invocation("supportsInterface")
+            .parameter(Type.Bytes(4)) { encode(tag.toByteArray()) },
+        Type.Bool::decode
+    )
 }
 
 internal fun RpcClient.erc165(address: Address) = ERC165(this, address)
---
+++ Shared/src/commonMain/kotlin/com/conradkramer/wallet/ethereum/abi/ERC721.kt
@@ -0,0 +1,22 @@
+package com.conradkramer.wallet.ethereum.abi
+
+import com.conradkramer.wallet.bigint.BigInteger
+import com.conradkramer.wallet.ethereum.Address
+import com.conradkramer.wallet.ethereum.RpcClient
+
+internal class ERC721Metadata(client: RpcClient, address: Address) : Contract(client, address) {
+    suspend fun name() = invoke(Invocation("name"), Type.String::decode)
+    suspend fun symbol() = invoke(Invocation("symbol"), Type.String::decode)
+
+    suspend fun tokenURI(tokenId: BigInteger) = invoke(
+        Invocation("tokenURI").parameter(Type.UInt()) { encode(tokenId) },
+        Type.String::decode
+    )
+}
+
+internal class ERC721Enumerable(client: RpcClient, address: Address) : Contract(client, address) {
+    suspend fun totalSupply() = invoke(Invocation("totalSupply"), Type.UInt()::decode)
+}
+
+internal fun RpcClient.erc721Metadata(address: Address) = ERC721Metadata(this, address)
+internal fun RpcClient.erc721Enumerable(address: Address) = ERC721Enumerable(this, address)
