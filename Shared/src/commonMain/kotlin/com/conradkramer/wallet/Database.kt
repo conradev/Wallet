ad5888c3d1a3d398d2e65b3df9b1b5e1ceff00dc
Conrad Kramer <conrad@conradkramer.com>
2023-01-11 13:12:11 -0500

    Create database schema to hold blockchain data
    This will be needed to view transaction history and balance information.

--- Shared/src/commonMain/kotlin/com/conradkramer/wallet/Database.kt
+++ Shared/src/commonMain/kotlin/com/conradkramer/wallet/Database.kt
@@ -7,18 +7,29 @@ import app.cash.sqldelight.db.SqlDriver
 import com.conradkramer.wallet.browser.BrowserPermissionStore
 import com.conradkramer.wallet.browser.prompt.Prompt
 import com.conradkramer.wallet.crypto.PublicKey
+import com.conradkramer.wallet.data.Account
 import com.conradkramer.wallet.data.Browser_permission
 import com.conradkramer.wallet.data.Browser_prompt
+import com.conradkramer.wallet.data.Erc20_contract
+import com.conradkramer.wallet.data.Erc721_contract
+import com.conradkramer.wallet.data.Eth_block
+import com.conradkramer.wallet.data.Eth_token_transfer
+import com.conradkramer.wallet.data.Eth_transaction
 import com.conradkramer.wallet.data.Public_key
+import com.conradkramer.wallet.ethereum.Address
+import com.conradkramer.wallet.ethereum.Chain
+import com.conradkramer.wallet.ethereum.Data
+import com.conradkramer.wallet.ethereum.Quantity
 import com.conradkramer.wallet.sql.Database
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.datetime.Instant
 
 typealias PublicKeyRecord = Public_key
-typealias AccountRecord = com.conradkramer.wallet.data.Account
+typealias AccountRecord = Account
 
 val Database.Companion.FILE_NAME: String
     get() = "Wallet.db"
@@ -38,12 +49,39 @@ private val coinAdapter = object : ColumnAdapter<Coin, Long> {
     override fun encode(value: Coin) = value.number
 }
 
-private val encodedAdapter = object : ColumnAdapter<PublicKey, ByteArray> {
+private val publicKeyAdapter = object : ColumnAdapter<PublicKey, ByteArray> {
     override fun decode(databaseValue: ByteArray) = PublicKey(databaseValue)
     override fun encode(value: PublicKey) = value.encoded(true)
 }
 
-internal fun Database.Companion.invoke(driver: SqlDriver): Database {
+private val dataAdapter = object : ColumnAdapter<Data, ByteArray> {
+    override fun decode(databaseValue: ByteArray) = Data(databaseValue)
+    override fun encode(value: Data) = value.data
+}
+
+private val addressAdapter = object : ColumnAdapter<Address, ByteArray> {
+    override fun decode(databaseValue: ByteArray): Address = Address(databaseValue)
+    override fun encode(value: Address): ByteArray = value.data
+}
+
+private val quantityAdapter = object : ColumnAdapter<Quantity, ByteArray> {
+    override fun decode(databaseValue: ByteArray) = Quantity(databaseValue)
+
+    // SQLiter seems to crash if we insert empty byte arrays
+    override fun encode(value: Quantity) = if (value.data.isEmpty()) ByteArray(1) else value.data
+}
+
+private val timestampAdapter = object : ColumnAdapter<Instant, String> {
+    override fun decode(databaseValue: String): Instant = Instant.parse(databaseValue)
+    override fun encode(value: Instant): String = value.toString()
+}
+
+private val chainAdapter = object : ColumnAdapter<Chain, Long> {
+    override fun decode(databaseValue: Long) = Chain(databaseValue)
+    override fun encode(value: Chain) = value.id
+}
+
+internal fun Database.Companion.withAdapters(driver: SqlDriver): Database {
     return Database(
         driver,
         browser_permissionAdapter = Browser_permission.Adapter(
@@ -52,9 +90,37 @@ internal fun Database.Companion.invoke(driver: SqlDriver): Database {
         browser_promptAdapter = Browser_prompt.Adapter(
             promptAdapter = promptAdapter
         ),
+        erc20_contractAdapter = Erc20_contract.Adapter(
+            chain_idAdapter = chainAdapter,
+            addressAdapter = addressAdapter,
+            total_supplyAdapter = quantityAdapter
+        ),
+        erc721_contractAdapter = Erc721_contract.Adapter(
+            chain_idAdapter = chainAdapter,
+            addressAdapter = addressAdapter,
+            total_supplyAdapter = quantityAdapter
+        ),
+        eth_blockAdapter = Eth_block.Adapter(
+            chain_idAdapter = chainAdapter,
+            timestampAdapter = timestampAdapter
+        ),
+        eth_transactionAdapter = Eth_transaction.Adapter(
+            chain_idAdapter = chainAdapter,
+            hashAdapter = dataAdapter,
+            fromAdapter = addressAdapter,
+            toAdapter = addressAdapter,
+            value_Adapter = quantityAdapter
+        ),
+        eth_token_transferAdapter = Eth_token_transfer.Adapter(
+            chain_idAdapter = chainAdapter,
+            contractAdapter = addressAdapter,
+            fromAdapter = addressAdapter,
+            toAdapter = addressAdapter,
+            value_Adapter = quantityAdapter
+        ),
         public_keyAdapter = Public_key.Adapter(
             coinAdapter = coinAdapter,
-            encodedAdapter = encodedAdapter
+            encodedAdapter = publicKeyAdapter
         )
     )
 }
