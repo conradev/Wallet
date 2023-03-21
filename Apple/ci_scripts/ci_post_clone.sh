1afc550ae64fc9fb7437c110b415c3a7169c8165
Conrad Kramer <conrad@conradkramer.com>
2023-03-20 23:44:41 -0400

    Move npm install to ci_post_clone
    This does not need to be run on machines configured for
    test-without-building.

--- Apple/ci_scripts/ci_post_clone.sh
+++ Apple/ci_scripts/ci_post_clone.sh
@@ -2,5 +2,7 @@
 
 brew install node openjdk@19
 
+npm install
+
 mkdir -p ~/Library/Java/JavaVirtualMachines
 ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk ~/Library/Java/JavaVirtualMachines/openjdk.jdk
