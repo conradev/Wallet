b11e055d8ce57839d1c4ebd297baefa0ed2c7ca3
Conrad Kramer <conrad@conradkramer.com>
2023-09-09 22:43:32 -0700

    Switch NPM to Bun for installing packages in CI

--- Apple/WebExtension/embed.sh
+++ Apple/WebExtension/embed.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Brew
-export PATH="$PATH:/opt/homebrew/bin:/opt/brew/bin"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. >/dev/null 2>&1 && pwd)"
 
-# Nix
-export PATH="$PATH:/etc/profiles/per-user/$USER/bin"
-if [[ -x $(command -v direnv) ]]; then
-    export PATH="${PATH}:$(direnv exec . sh -c 'echo $PATH')"
-fi
+export PATH="$PATH:$HOME/.bun/bin:$PROJECT_DIR/node_modules/.bin"
 
-npm run --prefix "${PROJECT_DIR}/../Extension" package
+cd "$PROJECT_DIR/Extension"
 
-BUILT_RESOURCES_DIR="${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
-mkdir -p "${BUILT_RESOURCES_DIR}"
-cp -R "${PROJECT_DIR}/../Extension/build/safari/" "${BUILT_RESOURCES_DIR}"
+bun install
+bun run package
+
+BUILT_RESOURCES_DIR="$BUILT_PRODUCTS_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
+mkdir -p "$BUILT_RESOURCES_DIR"
+cp -R "$PROJECT_DIR/Extension/build/safari/" "$BUILT_RESOURCES_DIR"
--- Apple/ci_scripts/ci_post_clone.sh
+++ Apple/ci_scripts/ci_post_clone.sh
@@ -2,7 +2,7 @@
 
 brew install node openjdk@19
 
-npm install
+bun install
 
 mkdir -p ~/Library/Java/JavaVirtualMachines
 ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk ~/Library/Java/JavaVirtualMachines/openjdk.jdk
